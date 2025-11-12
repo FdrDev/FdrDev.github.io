bc14c1ddba4ffbc09d25fb97251840f655705c54
FdrDev <federico.dente92@gmail.com>
2025-11-12 20:27:57 +0100

    Enhance HTML structure and styles; optimize particle effects for mobile

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 function typed() {
   var typed = new Typed("#typed", {
-  strings: ["&#60;&#8260;Hell0 W0rld&#62;","Ciao a tutti", "I'm Federico"],
+  strings: ["I'm Federico"],
   smartBackspace: true,
   typeSpeed:120,
   backSpeed:50
@@ -10,14 +10,98 @@ function typed() {
 }
 
 function particles(){
-  particlesJS.load('particles-js', 'particles.json', function(){
-    console.log('particles.json loaded...');
+  // Riduce il numero di particles su mobile
+  var particleCount = 150;
+  if (window.innerWidth <= 480) {
+    particleCount = 50;
+  } else if (window.innerWidth <= 768) {
+    particleCount = 80;
+  }
+
+  particlesJS('particles-js', {
+    "particles": {
+      "number": {
+        "value": particleCount,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 1,
+          "color": "#ccc"
+        }
+      },
+      "opacity": {
+        "value": 1,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 1
+        }
+      },
+      "size": {
+        "value": 2,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 30
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 120,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 2,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 140,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "push": {
+          "particles_nb": 4
+        }
+      }
+    },
+    "retina_detect": true
   });
 }
 
 
 function init(){
-  typed();
+  // typed();
   particles();
 }
 
